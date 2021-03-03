8d84cfeac58f2feb68859ee7d4e8b94453c1bb20
Luke Heeringa <lukeheeringa@gmail.com>
2021-03-03 15:16:58 -0500

    tensorflow cpu import

--- streamlit/classification_app.py
+++ streamlit/classification_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import numpy as np
-import tensorflow-cpu.keras
+import tensorflow.keras
 import librosa
 import sounddevice as sd
 import io
 from scipy.io.wavfile import write
 
-model = tensorflow-cpu.keras.models.load_model('../models/gru_model_split552.h5')
+model = tensorflow.keras.models.load_model('../models/gru_model_split552.h5')
 
 st.title('Audio Language Classification')
 
