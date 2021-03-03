8751432ca7aaa64485189c8a8f50d7dcac90d967
Luke Heeringa <lukeheeringa@gmail.com>
2021-03-03 14:54:16 -0500

    streamlit error

--- streamlit/classification_app.py
+++ streamlit/classification_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import numpy as np
-import tensorflow.keras
+import keras
 import librosa
 import sounddevice as sd
 import io
 from scipy.io.wavfile import write
 
-model = tensorflow.keras.models.load_model('../models/gru_model_split552.h5')
+model = keras.models.load_model('../models/gru_model_split552.h5')
 
 st.title('Audio Language Classification')
 
